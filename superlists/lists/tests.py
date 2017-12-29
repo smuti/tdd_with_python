73ece4f88cd1985bdcfd07566f5dfd4319024b12
Bilal ÇOLAK <bicolak@innova.com.tr>
2017-12-29 10:45:53 +0300

    added unittest:test_home_page_return_correct_html-status:red

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,9 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -8,3 +11,9 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
 
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
