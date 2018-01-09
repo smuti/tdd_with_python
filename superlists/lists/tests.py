6da2f3fbadb7925757fac7d25afd22dece2da73a
Bilal ÇOLAK <bicolak@innova.com.tr>
2018-01-09 11:16:58 +0300

    Added red status unit test for saving_and_retrieving_items in Model

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,14 +4,15 @@ from django.test import TestCase
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         
         self.assertEqual(found.func, home_page)
-
 
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
@@ -33,3 +34,23 @@ class HomePageTest(TestCase):
             {'new_item_text': 'A new list item'}, request=request
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first(ever) item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first(ever) item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
